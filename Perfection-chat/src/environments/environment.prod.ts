17a9a6824647017352e77efcd92aa7b451434fe1
donbetur <donbetur26@gmail.com>
2023-06-05 20:38:19 +0300

    railway back URL added

--- Perfection-chat/src/environments/environment.prod.ts
+++ Perfection-chat/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: true,
-  baseApiUrl: 'http://localhost:3000/api-chat'
+production: true,
+baseApiUrl: 'https://rkspback-production.up.railway.app/api-chat'
+//baseApiUrl: 'http://localhost:3000/api-chat'
 };
